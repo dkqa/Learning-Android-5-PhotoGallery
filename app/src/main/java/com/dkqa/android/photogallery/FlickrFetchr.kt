52ae9621a890e7b1f2c2ee00ace338fa5844159a
dkqa <dkqmoto@gmail.com>
2022-02-06 18:55:25 +0300

    Exercise - Chapter 24 - custom deserializer

--- app/src/main/java/com/dkqa/android/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/dkqa/android/photogallery/FlickrFetchr.kt
@@ -3,10 +3,8 @@ package com.dkqa.android.photogallery
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.dkqa.android.photogallery.api.FlickrApi
-import com.dkqa.android.photogallery.api.FlickrResponse
-import com.dkqa.android.photogallery.api.GalleryItem
-import com.dkqa.android.photogallery.api.PhotoResponse
+import com.dkqa.android.photogallery.api.*
+import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,9 +19,13 @@ class FlickrFetchr {
     private val flickrApi: FlickrApi
 
     init {
+        val gson = GsonBuilder()
+            .registerTypeAdapter(PhotoResponse::class.java, PhotoDeserializer())
+            .create()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
@@ -31,13 +33,12 @@ class FlickrFetchr {
 
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
+        val flickrRequest: Call<PhotoResponse> = flickrApi.fetchPhotos()
 
-        flickrRequest.enqueue(object : Callback<FlickrResponse> {
-            override fun onResponse(call: Call<FlickrResponse>, response: Response<FlickrResponse>) {
+        flickrRequest.enqueue(object : Callback<PhotoResponse> {
+            override fun onResponse(call: Call<PhotoResponse>, response: Response<PhotoResponse>) {
                 Log.d(TAG, "Response received")
-                val flickrResponse: FlickrResponse? = response.body()
-                val photoResponse: PhotoResponse? = flickrResponse?.photos
+                val photoResponse: PhotoResponse? = response.body()
                 var galleryItems: List<GalleryItem> = photoResponse?.galleryItems ?: mutableListOf()
                 galleryItems = galleryItems.filterNot {
                     it.url.isBlank()
@@ -45,7 +46,7 @@ class FlickrFetchr {
                 responseLiveData.value = galleryItems
             }
 
-            override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
+            override fun onFailure(call: Call<PhotoResponse>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch photos", t)
             }
 
---
+++ app/src/main/java/com/dkqa/android/photogallery/api/PhotoDeserializer.kt
@@ -0,0 +1,18 @@
+package com.dkqa.android.photogallery.api
+
+import com.google.gson.Gson
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
+
+class PhotoDeserializer : JsonDeserializer<PhotoResponse> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): PhotoResponse {
+        val jsonObject = json?.asJsonObject?.getAsJsonObject("photos")
+        return Gson().fromJson(jsonObject.toString(), PhotoResponse::class.java)
+    }
+}
