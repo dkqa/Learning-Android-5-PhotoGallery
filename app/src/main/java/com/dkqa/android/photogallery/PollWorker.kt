485431d1366390845760551d6a456fffeed9ebe4
dkqa <dkqmoto@gmail.com>
2022-02-20 21:17:33 +0300

    Chapter 27 - WorkManager - 616 page - Checking for new Photos

--- app/src/main/java/com/dkqa/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/dkqa/android/photogallery/PollWorker.kt
@@ -4,13 +4,41 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dkqa.android.photogallery.api.GalleryItem
 
 private const val TAG = "PallWorker"
 
 class PollWorker(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
 
     override fun doWork(): Result {
-        Log.i(TAG, "Work request triggered")
+        val query = QueryPreferences.getStoredQuery(context)
+        val lastResultId = QueryPreferences.getLastResultId(context)
+        val items: List<GalleryItem> = if (query.isEmpty()) {
+            FlickrFetchr().fetchPhotosRequest()
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } else {
+            FlickrFetchr().searchPhotosRequest(query)
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } ?: emptyList()
+
+        if (items.isEmpty()) {
+            return Result.success()
+        }
+
+        val resultId = items.first().id
+        if (resultId == lastResultId) {
+            Log.i(TAG, "Got an old result: $resultId")
+        } else {
+            Log.i(TAG, "Got a new result: $resultId")
+            QueryPreferences.setLastResultId(context, resultId)
+        }
+
         return Result.success()
     }
 
